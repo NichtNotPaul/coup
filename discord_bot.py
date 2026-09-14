1e8af502fb380771020cdd6674e947c80b5cccae
NichtNotPaul <Paulbrackmann@outlook.com>
2026-09-14 18:42:24 +0200

    Made bot send responses.

--- discord_bot.py
+++ discord_bot.py
@@ -8,8 +8,14 @@ def discord_bot():
             print(f'\033[0;34mSigned in as {self.user}\033[0m')
 
         async def on_message(self, message):
+
+            if message.author == self.user:
+                return
+            
             print(f'Message from {message.author}: {message.content}')
-            print(generate_response(f'Message from {message.author}: {message.content}'))
+
+            response = generate_response(f'Message from {message.author}: {message.content}')
+            await message.channel.send(response)
 
     intents = discord.Intents.default()
     intents.message_content = True
